1551bc0bbea635ac283146fe3d52a8f6a07bc6b1
0verkil <112268976+0verkil@users.noreply.github.com>
2026-09-18 22:36:12 -0700

    end of day code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/shooter/ShooterTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/shooter/ShooterTuner.java
@@ -5,9 +5,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Utility;
-import gay.zharel.fateweaver.flight.FateLogManager;
 import org.firstinspires.ftc.teamcode.util.Logger;
 
 import static dev.nextftc.units.Units.RotationsPerMinute;
@@ -38,6 +36,8 @@ public class ShooterTuner extends LinearOpMode {
 
             telemetry.addData("Shooter RPM", shooter.getCurrentVelocity().into(RotationsPerMinute));
             telemetry.addData("Target Shooter RPM", DESIRED_POWER_OR_RPM);
+
+            telemetry.update();
         }
     }
 
